07c0a877990c360c2e1a74c488320023200fb7c6
Bereket <bereketzewdu15@gmail.com>
2022-12-08 12:53:12 -0800

    Qustion 8 commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: 1 Success
+ *     */
+
+int main(void)
+		{
+				write(STDOUT_FILENO, "and that piece of art is useful", 31);
+					write(STDOUT_FILENO, "\" - Dora Korpar, 2015-10-19\n", 28);
+						return (1);
+		}
