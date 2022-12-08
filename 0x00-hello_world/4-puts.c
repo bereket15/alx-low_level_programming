53b27efb947217096f937ee237245b32e7652327
Bereket <bereketzewdu15@gmail.com>
2022-12-08 11:45:42 -0800

    Qustion 4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 /**
- * main -entery
- * Returen 0
+ * main - entery
+ * Return: Always 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	/**
+	 * this fun retern 0
+	 * have no out put
+	 */
 	return (0);
 }
