6a5edf763628bc93f5f51d7c17ee92b5693c97ed
Bereket <bereketzewdu15@gmail.com>
2022-12-08 13:07:51 -0800

    Qustion 8 commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
  */
 
 int main(void)
-        {
+{
         write(STDOUT_FILENO, "and that piece of art is useful", 31);
 	write(STDOUT_FILENO, "\" - Dora Korpar, 2015-10-19\n", 28);
 	return (1);
-	}
+}
