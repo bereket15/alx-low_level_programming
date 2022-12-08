f3f9eee23dd1363a0ace63456a7d9ce5413449cf
Bereket <bereketzewdu15@gmail.com>
2022-12-08 13:24:33 -0800

    Qustion 8 commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- * Return: 1 Success
+ * main - Entry
+ * Return: 1
  */
 int main(void)
 {
-    	 write(STDOUT_FILENO, "and that piece of art is useful", 31);
-	 write(STDOUT_FILENO, "\" - Dora Korpar, 2015-10-19\n", 28);
-	 return (1);
+	write(1,"and that piece of art is useful\"  -Dora korpar, 2015-10-19\n,59);
+	return (1);
 }
