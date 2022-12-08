ecc7fb0a07e771f926edd7fcbfcd0874bc07fc1c
Bereket <bereketzewdu15@gmail.com>
2022-12-08 14:13:44 -0800

    Qustion 8 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,18 +8,16 @@
 
 int main(void)
 		{
-				char ch;
-					int in;
-						long int lin;
-							long long int llin;
-								float fl;
+		char ch;
+		int in;
+		long int lin;
+		long long int llin;
+		float fl;
 
-									printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(ch));
-										printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(in));
-											printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(lin));
-												printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(llin));
-													printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(fl));
-
-														return (0);
+		printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(ch));
+		printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(in));
+                printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(lin))
+		printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(llin));
+                printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(fl));
+		return (0);
 		}
-
