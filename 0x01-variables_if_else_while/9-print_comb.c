4da082c49d5c6a2422e5313f339e1ec711005126
Bereket <bereketzewdu15@gmail.com>
2022-12-09 13:30:17 -0800

    commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; num < 10 ; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(',');
+		}
+	}
+		putchar('\n');
+		return (0);
+}
