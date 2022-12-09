10ab364dfb844810940d2907afd815e47a6ce66a
Bereket <bereketzewdu15@gmail.com>
2022-12-09 13:14:44 -0800

    commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	char b16;
+
+	for (n = 0 ; n <= 9 ; n++)
+		putchar((n % 10) + '0');
+	for (b16 = 'a' ; b16 <= 'f' ; b16++)
+		putchar(b16);
+	putchar('/n');
+	return (0);
+}
