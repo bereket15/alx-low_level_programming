ecece8697fe64f51d356405f2e1c6c1312f0981d
Bereket <bereketzewdu15@gmail.com>
2022-12-09 10:46:29 -0800

    Qustio 1 ed commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,9 +14,9 @@ int main(void)
 	y = n % 10;
 	if (n > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, y);
-	else if (n == 0)
-		printf("Last digit of %d is %d and is 0\n", n, y);
-	else
+	else if (n < 5 && n != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);
+	else
+		printf("Last digit of %d is %d and is 0\n", n, y);
 	return (0);
 }
