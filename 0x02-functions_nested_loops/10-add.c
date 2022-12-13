087953a8272286f20ba0f2163a54b409aceb99d0
Bereket <bereketzewdu15@gmail.com>
2022-12-13 13:54:51 -0800

    Qustion 7 commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - Function
+ * Description: print sum of two number
+ * @c: number one
+ * @d: number two
+ * Return: sum of two number
+ */
+int add(int c, int d)
+{
+	int a = c;
+	int b = d;
+	int s;
+
+	s = a + b;
+	return (s);
+}
