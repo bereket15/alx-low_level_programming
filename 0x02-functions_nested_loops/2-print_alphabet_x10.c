b9b20b1f620529160fcf1cb3d9fb9b8793214a72
Bereket <bereketzewdu15@gmail.com>
2022-12-13 11:54:17 -0800

    Qustion 2 commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print ten alphabet
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char lc;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (lc = 'a'; lc <= 'z'; lc++)
+			_putchar(lc);
+	}
+	_putchar('\n');
+}
