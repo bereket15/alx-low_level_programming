25c4203affa0e132e9793db56e7be7831003b896
Bereket <bereketzewdu15@gmail.com>
2022-12-13 15:21:55 -0800

    Q 101

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * Description: Print the first 98 fevonacci numbers
+ * Return: 0 - success
+ */
+int main(void)
+{
+	int i;
+	long int f1 = 0, f2 = 1, sum;
+
+	for (i = 0 ; i < 98 ; i++)
+	{
+		sum = f1 + f2;
+		printf("%li", sum);
+		f1 = f2;
+		f2 = sum;
+		if (i == 97)
+			printf("\n");
+		else
+												printf(", ");
+	}
+	return (0);
+}
