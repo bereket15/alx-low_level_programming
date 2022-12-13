7e9f816b4fd88bb441e16959774c26f8513b5ab3
Bereket <bereketzewdu15@gmail.com>
2022-12-13 14:04:23 -0800

    Qustion 7 commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_to_98 - function
+ * Description: Print natural numer to 99
+ * @n: Natural number
+ * Return: Always o
+ */
+void print_to_98(int n)
+{
+	for (int i = n; n <= 98; i++)
+		_putcher(n);
+	_putchar('\n');
+}
