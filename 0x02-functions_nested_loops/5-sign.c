9bd2221961ac497aad1961adad4ebf1bee4275ec
Bereket <bereketzewdu15@gmail.com>
2022-12-13 12:48:05 -0800

    Qustion 5 commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - Function
+ * Description: Checks whether a character is postive , zero or negitive
+ *  @n: is the passed value of the character to check
+ *  Return: 1,0,-1 according to n value
+ */
+int print_sign(int n)
+{
+	int nn = n;
+
+	if (nn > 0)
+	{
+		_putchar("+");
+		return (1);
+	}
+	if (nn == 0)
+	{
+		_putchar("0");
+		return (0);
+	}
+	else
+	{
+		_putchar("-");
+		return ("-1");
+	}
+}
