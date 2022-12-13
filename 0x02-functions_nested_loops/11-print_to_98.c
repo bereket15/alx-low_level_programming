a0a8f07807c915917d0eb442868a8976c4bec7bd
Bereket <bereketzewdu15@gmail.com>
2022-12-13 14:19:04 -0800

    Qustion 11 commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,9 +7,33 @@
  */
 void print_to_98(int n)
 {
-	int i = n;
+	int i;
 
-	for (i = n; i <= 98; i++)
-		_putchar(n);
-	_putchar('\n');
+	if (n < 98)
+	{
+		for (i = n ; i <= 98 ; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
+	else
+		if (n > 98)
+		{
+			for (i = n ; i >= 98 ; i--)
+			{
+				for (i = n ; i >= 98 ; i--)
+				{
+					printf("%d", i);
+					if (i != 98)
+						printf(", ");
+				}
+				printf("\n");
+			}
+			else
+				printf("%d\n", n);
+		}
 }
+
