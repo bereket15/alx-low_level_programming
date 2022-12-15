3c1c0859a87fe1c3e6966443661fd82eee0a5d3f
Bereket <bereketzewdu15@gmail.com>
2022-12-15 10:49:37 -0800

    Qustion 0 commit

---
+++ 0x03-debugging/0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - function
+ * Description: Checks whether a character is uppercase or not
+ * @c: is the passed value of the character to check
+ * Return: 1 - success 0 - not success
+ */
+int _isupper(int c)
+{
+	char ch = c;
+
+	if (ch >= 'A' && ch <= 'Z')
+		return (1);
+	else
+		return (0);
+}
