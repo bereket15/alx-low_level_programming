670cb0bee3cf688d9a595e13ee86936ba1d4434d
Bereket <bereketzewdu15@gmail.com>
2022-12-19 12:40:19 -0800

    Qustion one commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,9 +10,9 @@ void print_rev(char *s)
 	int i, j;
 
 	i = 0;
-	while (*(str + i) != '\0')
+	while (*(s + i) != '\0')
 		i++;
 	for (j = i - 1 ; j >= 0 ; j--)
-		_putchar(*(str + j));
+		_putchar(*(s + j));
 	_putchar('\n');
 }
