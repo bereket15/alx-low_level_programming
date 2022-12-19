96b44da15771c086c423e00e2e2eecf525c3fdab
Bereket <bereketzewdu15@gmail.com>
2022-12-19 11:43:18 -0800

    Qustion one commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h>"
+/**
+ * void reset_to_98 - function
+ * Description: update the value of n using pointer
+ * @n: varyabel to point at
+ * Return: return void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
