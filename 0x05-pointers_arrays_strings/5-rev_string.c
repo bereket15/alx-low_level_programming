c803838a5bd30c503fd9522ead4a1a349860fa5b
Bereket <bereketzewdu15@gmail.com>
2022-12-19 12:49:56 -0800

    Qustion one commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - function
+ * Discription - revers a given string
+ * @s: a string to be reversed
+ * Return: return noting
+ */
+void rev_string(char *s)
+{
+
+	int i, j, c;
+	char tmp;
+
+	c = 0;
+	while (*(s + c) != '\0')
+		c++;
+	j = c / 2;
+	c -= 1;
+	for (i = 0 ; i < j ; i++)
+	{
+		tmp = *(s + (c - i));
+		*(s + (c - i)) = *(s + 1);
+		*(s + i) = tmp;
+	}
+}
