2af5b8b33dd43215d79af7d9ac953ccb68f88b00
Bereket <bereketzewdu15@gmail.com>
2022-12-22 00:47:05 -0800

    hot fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,8 +7,28 @@
  * @n: Third param
  * Return: a appended string
  */
-char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, src, n);
+	int count1, count2, x, full_size, tmp;
+
+	count1 = 0;
+	count2 = 0;
+
+	while (*(dest + count1) != '\0')
+		count1++;
+	while (*(src + count2) != '\0')
+		count2++;
+	if (n < count2)
+		tmp = n;
+	else
+		tmp = count2;
+	full_size = count1 + tmp;
+	for (x = 0 ; x < tmp ; x++)
+	{
+		*(dest + count1) = *(src + x);
+		count1++;
+	}
+	if (n < count2)
+		*(dest + full_size) = '\0';
 	return (dest);
 }
