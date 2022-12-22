83e9d6cf922844d6b43ed2fa8a5e2aaa5b65595a
Bereket <bereketzewdu15@gmail.com>
2022-12-22 03:15:07 -0800

    Qustion 2 commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncpy - function
+ * Description function that copies a string
+ * @dest: first parme
+ * @scr: second parame
+ * @n: third parame
+ * Return: a string
+ */
+char *_strncpy(char *dest, char *src, int n);
+{
+	int len = 0, i = 0;
+
+	while(dest[len])
+	{
+		dest[len] = src[len]
+		len++;
+	}
+	return dest;
+}	
