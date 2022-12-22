5e920dcd3788710c69bbe935bf749522877738c1
Bereket <bereketzewdu15@gmail.com>
2022-12-22 03:52:27 -0800

    qustio 3 commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,8 +8,20 @@
  * @n: third parame
  * Return: a string
  */
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
 {
-	strncpy(dest, src, n);
+	int count2, x;
+
+	count2 = 0;
+	x = 0;
+	while (*(src + x) != '\0')
+	{
+		count2++;
+		x++;
+	}
+	for (x = 0 ; *(src + x) && x < n ; x++)
+		*(dest + x) = *(src + x);
+	for (x = count2 ; x < n ; x++)
+		*(dest + x) = '\0';
 	return (dest);
 }
