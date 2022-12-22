0ce8099ffb0e768481e94bfd9c37834591a44b06
Bereket <bereketzewdu15@gmail.com>
2022-12-22 05:09:49 -0800

    qustion 5 commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,18 +3,18 @@
 /**
  * string_toupper - function
  * Description: Convert lowercase to uppercase
- * @s: String value to be uppercase
+ * @x: String value to be uppercase
  * Return: changed string
  */
-char *string_toupper(char *s)
+char *string_toupper(char *x);
 {
 	int i = 0;
 
-	while (s[i])
+	while (x[i])
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-			s[i] -= 32;
-			i++;
+		if (x[i] >= 97 && x[i] <= 122)
+			x[i] = x[i] - 32;
+		i++;
 	}
-	return (s);
+	return (x);
 }
