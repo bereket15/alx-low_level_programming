4b6ffd50bf84d9483f4ea79ff553ded71b78f913
Bereket <bereketzewdu15@gmail.com>
2023-01-02 09:43:32 -0800

    qustion  1 commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main .h"
+#include <stddef.h>
+
+/**
+ * _strchar - function
+ * Description: search a character in a string
+ * @s: The address to the value of string
+ * @c: Character to search for
+ * Return: the addres of the first occernc of search character
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *loc = NULL;
+
+	i = 0;
+	while(s[i] >= '\0')
+	{
+		if (*(s + i) == c))
+		{
+			loc = &(*(s + i));
+			exit(0);
+		}
+		i++;
+	}
+	return (loc);
+}
