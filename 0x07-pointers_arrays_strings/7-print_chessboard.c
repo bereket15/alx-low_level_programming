efd3f2c8aad8e28d8983f4b10b52ccc5d9935142
Bereket <bereketzewdu15@gmail.com>
2023-01-02 11:20:05 -0800

    qustion 6 commit

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main .h"
+
+/**
+ * print_chessboard - function
+ * Description: to print chase board
+ * @a: The chase boared to be printed
+ * Return: noting
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, i2;
+
+	for (i = 0; a[i][7]; i++)
+	{
+		for (i2 = 0; i2 < 8; i2++)
+			_putchar(a[i1][i2];
+		_putchar('\n');
+	}
+}
