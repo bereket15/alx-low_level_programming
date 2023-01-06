b3c882eae86a94eb1c30b6a437407ad7412da501
Bereket <bereketzewdu15@gmail.com>
2023-01-06 15:52:11 -0800

    Qustion 0 commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdioh>
+
+/**
+ * main -  this is a function that prints its name
+ * @argc: argc parameter
+ * @argv: an array of a comand
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
