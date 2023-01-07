b945e825d40a127b05bc1aaa9f933ba036961547
Bereket <bereketzewdu15@gmail.com>
2023-01-06 16:46:39 -0800

    Qustion 0 commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - this is a function
+ * @argc: argc parameter
+ * @argv: an array
+ * Return: 0 for correct out put
+ */
+int main(int argc, char *argv[])
+{
+	int result = 0, num1, num2;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+	return (0);
+}
