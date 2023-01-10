94e8a9f0ea93edb3a505f6d9d741d25d407457d3
Bereket <bereketzewdu15@gmail.com>
2023-01-10 10:51:08 -0800

    Qustionzero commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,8 +3,20 @@
 /**
  * 0-create_array.c - function
  * @c: first parameter
+ * @size: size of arry to be created
  * Return: a pointer
  */
 char *create_array(unsigned int size, char c)
 {
+	int i;
+	char *s;
 
+	if (size == 0)
+		return (NULL);
+	s = malloc(size * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		s[i] = c;
+	return (s);
+}
